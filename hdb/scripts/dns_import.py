f0feb51660690d9eb26b90b2bf6530cb5487fa5e
William Brown <william.e.brown@adelaide.edu.au>
2011-03-20 13:26:34 +1030

    added some automatic relationship mapping, and constraints to stop duplicate record addition

--- hdb/scripts/dns_import.py
+++ hdb/scripts/dns_import.py
@@ -44,32 +44,35 @@ for rtype in ('A', 'AAAA', 'MX', 'PTR', 'TXT', 'SRV', 'CNAME', 'NS', 'HINFO'):
 				else:
 					print r + ':' + rtype + ':' + rec[0][0] + ' ' + rec[0][1]
 				#Check if the record exists or not
-				dr = DNSRecord()
-				dr.zone = dnsz
-				dr.type = rtype
-				#Make this work for MX records
-				dr.record = rec
-				dr.ttl = dnsz.ttl
-				dr.fqdn = r
-				if rtype is 'A' or rtype is 'AAAA':
-					if len(Address.objects.filter(address=rec)) is 0:
-						#Then we need to create it.
-						a = Address()
-						a.host = None
-						a.type = 6
-						if rtype is 'A':
-							a.type = 4
-						a.vlan = 0
-						a.mac = None
-						a.address = rec
-						a.save()
-						dr.address = a
-				if rtype is 'MX' or rtype is 'CNAME' or rtype is 'NS':
-					#Add relation to the relevant A / AAAA records. May need to reparse the relevant tree for this. How do we handle many A records ... ?
-					pass
-				dr.save()
-				#Check if this object exists in our model (host, address and type)
-				#If we find a host by this FQDN, tie the address to it. Else skip and add address / record.
+				if len(DNSRecord.objects.filter(type=rtype,record=rec,fqdn=r)) is 0:
+					dr = DNSRecord()
+					dr.zone = dnsz
+					dr.type = rtype
+					#Make this work for MX records
+					dr.record = rec
+					dr.ttl = dnsz.ttl
+					dr.fqdn = r
+					if rtype is 'A' or rtype is 'AAAA':
+						if len(Address.objects.filter(address=rec)) is 0:
+							#Then we need to create it.
+							a = Address()
+							a.host = None
+							a.type = 6
+							if rtype is 'A':
+								a.type = 4
+							a.vlan = 0
+							a.mac = None
+							a.address = rec
+							a.save()
+							dr.address = a
+					if rtype is 'MX' or rtype is 'CNAME' or rtype is 'NS':
+						related = DNSRecord.objects.filter(Q(fqdn=r) , Q(type='A') | Q(type='AAAA'))
+						if len(related) is not 0:
+							for x in related:
+								dr.dnsrecord.add(x)
+					dr.save()
+					#Check if this object exists in our model (host, address and type)
+					#If we find a host by this FQDN, tie the address to it. Else skip and add address / record.
 		except ValueError as e:
 		#	#pass
 			print 'EXCEPTION ON:' + r + ':' + rtype + ' ;;ValueError; ' + e.message
