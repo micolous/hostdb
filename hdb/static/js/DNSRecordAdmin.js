2e5811ced16ff5c7def1a6cb7eb835401e108557
twoolie <woolford.thomas@gmail.com>
2011-06-19 19:48:04 +0930

    fixed trigger

--- hdb/static/js/DNSRecordAdmin.js
+++ hdb/static/js/DNSRecordAdmin.js
@@ -4,7 +4,12 @@
 		$(".form-row.record").hide();
 		$(".form-row.address").hide();
 		$("#id_type").change(function() {
-			if (this.value == "A" || this.value == "AAAA" || this.value == "PTR")
+			if (this.value == "" || this.value = null)
+			{
+				$(".form-row.record").slideUp(speed);
+				$(".form-row.address").slideUp(speed);
+			}
+			else if (this.value == "A" || this.value == "AAAA" || this.value == "PTR")
 			{
 				$(".form-row.record").slideUp(speed);
 				$(".form-row.address").slideDown(speed);
@@ -12,6 +17,6 @@
 				$(".form-row.record").slideDown(speed);
 				$(".form-row.address").slideUp(speed);
 			}
-		})
+		}).trigger('change')
 	})
 })(django.jQuery)
